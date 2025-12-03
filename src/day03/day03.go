2803624de5926efd7f629521a2fca37d037d9506
Ellie Chapman <ellie@amadido.co.uk>
2025-12-03 10:47:18 -0500

    replace wrappers with lambda functions

--- src/day03/day03.go
+++ src/day03/day03.go
@@ -2,45 +2,28 @@ package day2
 
 import (
 	"AoC_2025/src/utils"
-	"fmt"
 	"strings"
 )
 
 func Day3_part1(input []string) int {
-	banks := utils.Map(input, splitBatteries)
-	joltages := utils.Map(banks, getMaxJoltage2Wrapper)
+	banks := utils.Map(input, func(bank string) []int { return utils.Map(strings.Split(bank, ""), utils.Atoi) })
+	joltages := utils.Map(banks, func(b []int) int { return getMaxJoltageN(b, 2, 0) })
 	return utils.Sum(joltages)
 }
 
 func Day3_part2(input []string) int {
-	banks := utils.Map(input, splitBatteries)
-	joltages := utils.Map(banks, getMaxJoltage12Wrapper)
+	banks := utils.Map(input, func(bank string) []int { return utils.Map(strings.Split(bank, ""), utils.Atoi) })
+	joltages := utils.Map(banks, func(b []int) int { return getMaxJoltageN(b, 12, 0) })
 	return utils.Sum(joltages)
 }
 
-func splitBatteries(bank string) []int {
-	return utils.Map(strings.Split(bank, ""), utils.Atoi)
-}
-
-func getMaxJoltage2Wrapper(bank []int) int {
-	return getMaxJoltageN(bank, 2, 0)
-}
-
-func getMaxJoltage12Wrapper(bank []int) int {
-	return getMaxJoltageN(bank, 12, 0)
-}
-
-// EBC how can I directly use utils.Map with getMaxJoltageN?
-// Currently can't because it needs other parameters, not just the one being mapped over, so need wrapper functions
 func getMaxJoltageN(bank []int, numLeftToFind int, found int) int {
 	next, pos := findBiggest(bank[0 : len(bank)-numLeftToFind+1])
 	newFound := found*10 + next
 	if numLeftToFind == 1 {
-		// we are done, just found the last digit
-		return newFound
+		return newFound // we are done, just found the last digit
 	} else {
-		// more digits to find, recurse
-		return getMaxJoltageN(bank[pos+1:], numLeftToFind-1, newFound)
+		return getMaxJoltageN(bank[pos+1:], numLeftToFind-1, newFound) // More digits to find, recurse
 	}
 }
 
@@ -52,6 +35,5 @@ func findBiggest(bank []int) (int, int) {
 			}
 		}
 	}
-	fmt.Println("should never hit here")
-	return 0, 0
+	panic("Didn't match digit of any value")
 }
